32ac3373e2b94ae820ff8120724d2c99f7b69344
MarcNader <marknader1998@gmail.com>
2023-04-29 18:58:55 +0200

    Fixed a Bug

--- vite-project/src/Components/NavBar/navbar.component.jsx
+++ vite-project/src/Components/NavBar/navbar.component.jsx
@@ -1,29 +1,20 @@
-import "./navbar.styles.scss"
-import SearchBox from "../../../../monsters-rolodex/src/Components/search-box/search-box.component"
-import Notification from "../../assets/Vector.svg"
-import ProfilePicture from "../../assets/profilepicture.svg"
-import { useState } from "react"
+import "./navbar.styles.scss";
+import SearchBox from "../Search-Box/search-box.component";
+import Notification from "../../assets/Vector.svg";
+import ProfilePicture from "../../assets/profilepicture.svg";
+import { useState } from "react";
 
 const NavBar = () => {
-    const [placeholder,setplaceholder]=useState("Search..");
-return (
-
+  const [placeholder, setplaceholder] = useState("Search..");
+  return (
     <div className="Navbar">
+      <SearchBox placeholder={placeholder} />
+      <div className="third-section">
+        <img src={Notification} className="Notification-Icon" />
+        <img src={ProfilePicture} alt="no dp" className="profile-picture" />
+      </div>
+    </div>
+  );
+};
 
-    <SearchBox placeholder={placeholder} />
-<div className="third-section">
-<img src={Notification} className="Notification-Icon" />
-<img src={ProfilePicture} alt="no dp" className="profile-picture" />
-</div>
-
-</div>
-
-
-)
-
-
-
-
-}
-
-export default NavBar
+export default NavBar;
